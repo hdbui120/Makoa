0d545eae6aae4b126f29bed0bf0da4caf23be86e
Hieueee QED <hieubui457@gmail.com>
2017-01-04 22:21:48 -0800

    added opencv library and switch variable type from VictorSP to SpeedController

--- src/org/usfirst/frc/team4619/robot/subsystems/DriveBaseSubsys.java
+++ src/org/usfirst/frc/team4619/robot/subsystems/DriveBaseSubsys.java
@@ -3,8 +3,8 @@ package org.usfirst.frc.team4619.robot.subsystems;
 import org.usfirst.frc.team4619.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.VictorSP;
-import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,19 +14,18 @@ public class DriveBaseSubsys extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	private VictorSP frontLeft;
-	private VictorSP backLeft; 
-	private VictorSP frontRight; 
-	private VictorSP backRight; 
+	private SpeedController frontLeft;
+	private SpeedController backLeft; 
+	private SpeedController frontRight; 
+	private SpeedController backRight; 
 	
-	public DriveBaseSubsys()
+	public DriveBaseSubsys(SpeedController fl, SpeedController bl,
+							SpeedController fr, SpeedController br)
 	{
-		frontLeft = new VictorSP(RobotMap.frontLeftDrive);
-		backLeft = new VictorSP(RobotMap.backLeftDrive);
-		frontRight = new VictorSP(RobotMap.frontRightDrive);
-		backRight = new VictorSP(RobotMap.backRightDrive);
-		
-		RobotDrive driveTrain = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+		frontLeft = fl;
+		backLeft = bl;
+		frontRight = fr;
+		backRight = br;
 	}
 	
     public void initDefaultCommand() {
